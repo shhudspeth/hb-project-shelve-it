b713de7a95295468ff01998f0c190c34a77f774f
Sarah Hudspeth <shhudspeth@gmail.com>
2020-08-11 18:01:19 -0700

    updated data models

---
+++ data/BookShelveIt (1).sql
@@ -0,0 +1,110 @@
+CREATE TABLE "users" (
+  "user_id" SERIAL PRIMARY KEY,
+  "user_name" varchar,
+  "email" varchar,
+  "password" varchar,
+  "joined_at" datetime,
+  "profile_link" varchar,
+  "text_number" int,
+  "contact_by_email" boolean,
+  "contact_by_phone" boolean,
+  "share_link" varchar,
+  "lat" float,
+  "long" float,
+  "user_book_source_id" int
+);
+
+CREATE TABLE "bookshelf" (
+  "shelf_id" SERIAL PRIMARY KEY,
+  "user_id" int,
+  "nickname" varchar,
+  "sent_to_user" boolean,
+  "public_display" boolean
+);
+
+CREATE TABLE "shelvedBook" (
+  "bookshelfBook_id" SERIAL PRIMARY KEY,
+  "shelf_id" int,
+  "book_id" int,
+  "reading_status" int,
+  "owned_status" int,
+  "color_palette_id" int
+);
+
+CREATE TABLE "reading_status" (
+  "reading_id" [pk],
+  "reading_status_name" varchar
+);
+
+CREATE TABLE "owned_status" (
+  "owned_id" SERIAL PRIMARY KEY,
+  "owned_text" varchar
+);
+
+CREATE TABLE "color_schemes" (
+  "color_palette_id" SERIAL PRIMARY KEY,
+  "color_nums" int
+);
+
+CREATE TABLE "book" (
+  "book_id" SERIAL PRIMARY KEY,
+  "isbn" int,
+  "title" varchar,
+  "author" varchar,
+  "publisher" varchar,
+  "year_published" int,
+  "frontcover_img_source" varchar,
+  "description" varchar,
+  "spine_img_source" varchar
+);
+
+CREATE TABLE "genre" (
+  "genre_id" SERIAL PRIMARY KEY,
+  "name" varchar
+);
+
+CREATE TABLE "bookgenre" (
+  "bookgenre_id" SERIAL PRIMARY KEY,
+  "book_id" int,
+  "genre_id" int
+);
+
+CREATE TABLE "userBookSources" (
+  "user_book_source_id" SERIAL PRIMARY KEY,
+  "user_libraries_id" int,
+  "user_bookstores_id" int
+);
+
+CREATE TABLE "library" (
+  "library_id" SERIAL PRIMARY KEY,
+  "link_library" varchar,
+  "search_api_format" varchar
+);
+
+CREATE TABLE "indieBookstore" (
+  "bookstore_id" SERIAL PRIMARY KEY,
+  "link_bookstore" varchar,
+  "search_api_format" varcahr
+);
+
+ALTER TABLE "users" ADD FOREIGN KEY ("user_book_source_id") REFERENCES "userBookSources" ("user_book_source_id");
+
+ALTER TABLE "bookshelf" ADD FOREIGN KEY ("user_id") REFERENCES "users" ("user_id");
+
+ALTER TABLE "shelvedBook" ADD FOREIGN KEY ("shelf_id") REFERENCES "bookshelf" ("shelf_id");
+
+ALTER TABLE "shelvedBook" ADD FOREIGN KEY ("book_id") REFERENCES "book" ("book_id");
+
+ALTER TABLE "shelvedBook" ADD FOREIGN KEY ("reading_status") REFERENCES "reading_status" ("reading_id");
+
+ALTER TABLE "shelvedBook" ADD FOREIGN KEY ("owned_status") REFERENCES "owned_status" ("owned_id");
+
+ALTER TABLE "shelvedBook" ADD FOREIGN KEY ("color_palette_id") REFERENCES "color_schemes" ("color_palette_id");
+
+ALTER TABLE "bookgenre" ADD FOREIGN KEY ("book_id") REFERENCES "book" ("book_id");
+
+ALTER TABLE "bookgenre" ADD FOREIGN KEY ("genre_id") REFERENCES "genre" ("genre_id");
+
+ALTER TABLE "userBookSources" ADD FOREIGN KEY ("user_libraries_id") REFERENCES "library" ("library_id");
+
+ALTER TABLE "userBookSources" ADD FOREIGN KEY ("user_bookstores_id") REFERENCES "indieBookstore" ("bookstore_id");
--- model.py
+++ model.py
@@ -2,7 +2,7 @@ from flask_sqlalchemy import SQLAlchemy
 import flask
 from sqlachemy_utils import PasswordType, force_auto_coercion
 
-# Instantiate a SQLAlchemy object. We need this to create our db.Model classes.
+# Instantiate a SQLAlchemy object to create db.Model classes.
 db = SQLAlchemy()
 
     # force_auto_coercion()
@@ -19,21 +19,9 @@ db = SQLAlchemy()
 ##############################################################################
 # PART 1: COMPOSE ORM
 
-# Table users {
-#   user_id int [pk, increment] // auto-increment
-#   email varchar
-#   password varchar
-#   profile_link varchar
-#   text_number int
-#   contact_by_email boolean
-#   contact_by_phone boolean
-#   local_library int [ref: > libraries.library_id]
-#   local_bookstore int [ref: > indieBookstores.bookstore_id]
-# }
-
-
+# Table users 
 class User(db.Model):
-    """ data model for a user """
+    """ Data model for a user """
 
     __tablename__ = "users"
     
@@ -47,29 +35,20 @@ class User(db.Model):
     text_number = db.Column(db.String(100))
     contact_by_email = db.Column(db.Boolean)
     contact_by_text = db.Column(db.Boolean)
+    share_link = db.Column(varchar(100))
+    lat = db.Column(db.Float)
+    long = db.Column(db.Float)
 
-    bookcase = db.relationship('Bookcase')
+    # back references to other models/FK
+    bookshelves = db.relationship('Bookshelf')
 
+    # REPR
     def __repr__(self):
             """Return a human-readable representation of a User."""
             return f"<User id ={self.user_id} name ={self.user_name} email = {self.email}>"
 
 
 # Table book {
-#  book_id int [pk, increment] // auto-increment
-#   isbn int
-#   title varchar
-#   author varchar
-#   publisher varchar
-#   year_published int
-#   img_source varchar
-#   description varchar
-#   genre_book_id int [ref: > genre.genre_id]
-#   spine_image_source varchar
-#   color_palate_id int [ref: > color_schemes.color_palate_id]
-# }
-
-
 class Book(db.Model):
     """ Data Model for a Book """ 
 
@@ -81,35 +60,22 @@ class Book(db.Model):
     title = db.Column(db.String(30), nullable=False, unique=True)
     author= db.Column(db.String(30), nullable=False)
     publisher= db.Column(db.String(30) )
-    img_source = db.Column(db.String(100))
-    description = db.Column(db.String(100))
     year_published = db.Column(db.Integer)
-    want_to_read  = db.Column(db.Boolean)
-    have_already_read = db.Column(db.Boolean)
-    ordered = db.Column(db.Boolean)
-    liked = db.Column(db.Boolean)
-    didnot_like = db.Column(db.Boolean)
-    removed_from_shelf boolean = db.Column(db.Boolean)
-
-     bookshelves = db.relationship('Bookshelf')
+    cover_img_source = db.Column(db.String(100))
+    description = db.Column(db.String(100))
+    spine_img_source = db.Column(db.String(100))
+    
+    # back references to other models/FK
+    bookshelves = db.relationship('shelvedBook')
   
+    # REPR
     def __repr__(self):
             """Return a human-readable representation of a Book."""
             return f"<Book id ={self.book_id} title ={self.title} author = {self.author}>"
 
 
 
-# Table bookshelf {
- 
-#   shelf_id int [pk, increment] // auto-increment
-#   book_id int [ref: < book.book_id]
-#   sent_to_user boolean
-#   
-#  
-#   public_display boolean
-  
-# }
-
+# Table bookshelf 
 class Bookshelf(db.Model):
     """ Data Model for a Bookshelf """ 
 
@@ -118,44 +84,80 @@ class Bookshelf(db.Model):
     # Definition of Columns and relationships
     shelf_id = db.Column(db.Integer, primary_key=True, nullable=False, autoincrement=True)
     book_id = db.Column(db.Integer, db.ForeignKey('books.book_id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'))
     nickname = db.Column(db.String(30), nullable=False)
     sent_to_user = db.Column(db.Boolean)
-   
-
-    books = db.relationship('Book')
-  
-    def __repr__(self):
-            """Return a human-readable representation of a Bookshelf."""
-            return f"<Bookshelf id ={self.book_id} name ={self.nickname} sent = {self.sent_to_user}>"
-
-    
-# Table bookcase {
-#   bookcase_id int [pk, increment] //auto-increment
-#   shelf_id int [ref: < bookshelf.shelf_id]
-#    user_id int [ ref: > users.user_id]
-#    share_link varchar
-#    public_display boolean
-# }
-
-class Bookcase(db.Model):
-    """ Data Model for a Bookcase (a collection of shelves) """ 
-
-    __tablename__ = "bookcases"
-    
-    # Definition of Columns and relationships
-    bookcase_id = db.Column(db.Integer, primary_key=True, nullable=False, autoincrement=True)
-    shelf_id = db.Column(db.Integer, db.ForeignKey('bookshelves.shelf_id'))
-    user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'))
-    nickname = db.Column(db.String(30))
-    share_link = db.Column(db.String(100))
     public_display = db.Column(db.Boolean)
    
-    bookshelves = db.relationship('Bookshelf')
-
-    user = db.relationship('User')
+   # back references to other models/FK
+    users = db.relationship('User')
+    books = db.relationship('Book')
   
+    #REPR
     def __repr__(self):
-            """Return a human-readable representation of a Book."""
-            return f"<Bookcase_id ={self.book_id} name ={self.user.user_name} bookshelves = {self.bookselves}>"
+            """Return a human-readable representation of a Bookshelf."""
+            return f"<Bookshelf_id ={self.book_id} name ={self.nickname} sent = {self.sent_to_user}>"
 
     
+# Table shelvedBook (book<>BookShelf)
+class shelvedBook(db.Model):
+    """ Data Model for a book that is on a shelf """ 
+
+    __tablename__ = "shelved_books"
+    
+    # Definition of Columns and relationships
+    bookshelfBook_id = db.Column(db.Integer, primary_key=True, nullable=False, autoincrement=True)
+    shelf_id = db.Column(db.Integer, db.ForeignKey('bookshelves.shelf_id'))
+    book_id = db.Column(db.Integer, db.ForeignKey('books.book_id'))
+    
+   # back references to other models/FK
+    bookshelves = db.relationship('Bookshelf')
+    books = db.relationship('Book')
+  
+    # REPR
+    def __repr__(self):
+            """Return a human-readable representation of a Book."""
+            return f"<shelvedBook_id ={self.bookshelfBook_id} name ={self.user.user_name} bookshelves = {self.shelf_id}>"
+
+
+class reading_status(db.Model):
+    """ Data Model for a book that is on a shelf """ 
+
+    __tablename__ = "reading_status"
+    
+    # Definition of Columns and relationships
+    reading_status_id = db.Column(db.Integer, primary_key=True, nullable=False, autoincrement=True)
+    reading_status_name = db.Column(db.String(30), unique=True)
+
+
+    # REPR
+    def __repr__(self):
+            """Return a human-readable representation of a Book."""
+            return f"< Reading_status ={self.reading_status_name} >"
+
+class owned_status(db.Model):
+    """ Data Model for a book that is on a shelf """ 
+
+    __tablename__ = "owned_status"
+    
+    # Definition of Columns and relationships
+    owned_id = db.Column(db.Integer, primary_key=True, nullable=False, autoincrement=True)
+    owned_text = db.Column(db.String(30), unique=True)
+
+
+    # REPR
+    def __repr__(self):
+            """Return a human-readable representation of a Book."""
+            return f"< Owned_status ={self.owned_text} >"
+
+
+
+    # nickname = db.Column(db.String(30))
+    # share_link = db.Column(db.String(100))
+    # public_display = db.Column(db.Boolean)
+    # want_to_read  = db.Column(db.Boolean)
+    # have_already_read = db.Column(db.Boolean)
+    # ordered = db.Column(db.Boolean)
+    # liked = db.Column(db.Boolean)
+    # didnot_like = db.Column(db.Boolean)
+    # removed_from_shelf boolean = db.Column(db.Boolean)
